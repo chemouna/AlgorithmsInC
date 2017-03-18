b53d66aca0068aae258988e3853b7abceeb8b1e0
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-18 12:44:49 +0100

    KiloMax implemented in C++

---
+++ KiloManX.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Mouna Cheikhna on 18/03/2017.
+//
+
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <iterator>
+
+using namespace std;
+
+struct Node {
+    Node(int weapons, int shots)
+            : weapons_(weapons), shots_(shots) {}
+
+    int weapons_;
+    int shots_;
+};
+
+int leastShots(const vector<string> &damageChart, const vector<int> &bossHealth) {
+
+    const int n = (const int) damageChart.size();
+    const int numWeapons = (const int) damageChart[0].size();
+    vector<bool> visited(32768, false);
+    priority_queue<Node> pq;
+    pq.push(Node(0, 0));
+
+    while (!pq.empty()) {
+        Node top = pq.top();
+
+        if (visited[top.weapons_]) continue;
+        visited[top.weapons_] = true;
+
+        //check if target reached : all bosses defeated which is equivalent to having all the weapons
+        if (top.weapons_ == (1 << numWeapons) - 1) {
+            return top.shots_;
+        }
+
+        //relax step
+        for (int i = 0; i < damageChart.size(); i++) {
+            if ((i << numWeapons) & 1) continue;
+
+            // Figure out what the best amount of time that we can destroy this boss is, given the weapons we have.
+            // We initialize this value to the boss's health, as that is our default (with KiloBuster).
+            // this equivalent to the step where we compare distances in classic Dijkstra and pick the min one
+
+            int best = bossHealth[i];
+
+            for (int j = 0; j < damageChart.size(); j++) {
+
+                if (i == j) continue; // ?
+
+                //if i've got this weapon (its bit is set on my weapons field) and it has power (its not '0')
+                //then i can use it
+                if (((j << top.weapons_) & 1) && damageChart[j][i] != '0') {
+                    //we have the weapon so lets defeat the boss with it
+                    int shotsNeeded = bossHealth[i] / (damageChart[j][i] - '0'); //?
+                    if (bossHealth[i] % damageChart[j][i] != '0') { // ?
+                        shotsNeeded++;
+                    }
+                    best = min(best, shotsNeeded);
+                }
+            }
+            // Decrease operation : Add the new node to be searched, showing that we defeated boss i and so we have
+            //  his weapons now (==> we union weapons bits field with 1 << i) and we used 'best' shots to defeat him.
+            pq.push(Node(top.weapons_ | 1 << i, top.shots_ + best));
+        }
+    }
+
+    return -1;
+}
+
+
