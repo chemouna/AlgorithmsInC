98e02030beceaf7cfd4823c0bc9478632f786ba2
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-23 07:34:32 +0100

    Trie.

---
+++ Trie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mouna Cheikhna on 23/10/2017.
+//
+
+#include "Trie.h"
---
+++ Trie.h
@@ -0,0 +1,14 @@
+//
+// Created by Mouna Cheikhna on 23/10/2017.
+//
+
+#ifndef ALGORITHMSINC_TRIE_H
+#define ALGORITHMSINC_TRIE_H
+
+
+class Trie {
+
+};
+
+
+#endif //ALGORITHMSINC_TRIE_H
